905a6197e11a8825182dc099870565241891faa6
Varshaa0211 <126751546+Varshaa0211@users.noreply.github.com>
2025-09-16 17:08:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,20 @@ model = joblib.load('house_price_model.pkl')
 
 st.title('House Price Prediction')
 
-sqft = st.number_input('Enter sqft_living')
-bedrooms = st.number_input('Enter number of bedrooms')
-bathrooms = st.number_input('Enter number of bathrooms')
-floors = st.number_input('Enter number of floors')
+sqft_living = st.number_input('Enter sqft_living')
+bedrooms = st.number_input('Enter bedrooms')
+bathrooms = st.number_input('Enter bathrooms')
+floors = st.number_input('Enter floors')
+feature_5 = st.number_input('Enter feature_5')
+...
+feature_18 = st.number_input('Enter feature_18')
+
+if st.button('Predict'):
+    features = np.array([[sqft_living, bedrooms, bathrooms, floors, feature_5, ..., feature_18]])
+    prediction = model.predict(features)[0]
+    st.success(f'Predicted Price: ${prediction:,.2f}')
+
+
 
 if st.button('Predict Price'):
     features = np.array([[sqft, bedrooms, bathrooms, floors]])
